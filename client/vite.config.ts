8b71d93bca1cdadd1dbdb04f2c86897bc53d2f39
justinfei9 <jpf99@yahoo.com>
2024-11-25 14:22:31 -0500

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import vueRouter from 'unplugin-vue-router/vite'
+import VueRouter from 'unplugin-vue-router/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vueRouter({
+    VueRouter({
       /* options */
     }),
     vue(),
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+export default {
+  // ...existing code...
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "./index.html",
+    },
+  },
+  // ...existing code...
+};
